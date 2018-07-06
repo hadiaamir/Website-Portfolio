e2299a5384a8842f7d5a39f1b1c4154464d2a4cd
Hadi Aamir <35180405+hadiaamir@users.noreply.github.com>
2018-07-06 17:41:11 +0500

    Updated version

---
+++ Assets/Action/action.js
@@ -0,0 +1,30 @@
+
+      // Menu-toggle button
+
+      $(document).ready(function() {
+            $(".menu-icon").on("click", function() {
+                  $("nav ul").toggleClass("showing");
+            });
+      });
+
+      // Scrolling Effect
+
+      $(window).on("scroll", function() {
+            if($(window).scrollTop()) {
+                  $('nav').addClass('black');
+            }
+
+            else {
+                  $('nav').removeClass('black');
+            }
+      })
+
+		$(document).ready(function() {
+		  $('a[href^="#"]').click(function() {
+		      var hash = $(this).attr('href');
+		        $('html, body').animate({
+		          scrollTop: $(hash).offset().top
+		        }, 1500);
+		        return false;
+		    });
+		});
