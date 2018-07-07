41ec4a9e935f0da4fe600ab4da8e8f0161f4b2aa
Hadi Aamir <hadi.aamir8@gmail.com>
2018-07-07 13:45:16 +0500

    update

--- Action/scroll.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('a[href^="#"]').click(function() {
-      var hash = $(this).attr('href');
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 1500);
-        return false;
-    });
-});
